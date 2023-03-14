9d249bb4ed46524831e58cb4885f21fad5a31f7e
bantealem <bantem2021@gmail.com>
2023-03-14 10:46:30 +0300

    Add index js

---
+++ src/redux/reducers/index.jsx
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import { productReducers } from './productReducer';
+
+export const reducers = combineReducers({
+    allProducts: productReducers,
+});
+
+
